bc0feda3c9bf83388d3cb8be20283954a511b47b
Israelnjor <ibkiche@st.ug.edu.gh>
2026-02-09 19:00:41 +0000

    Defined attributes and added Constructor

--- ATM.java
+++ ATM.java
@@ -1,1 +1,20 @@
+public class ATM {
+  private final String username;
+  private final int accountNumber;
+  private int pin;
+  private double balance;
+  private double dailyLimit;
+  private double withdrawnToday;
 
+
+public ATM(String username, int accountNumber, int pin, double balance, double dailyLimit, double withdrawnToday) {
+  this.username = username;
+  this.accountNumber = accountNumber;
+  this.pin = pin;
+  this.balance = balance;
+  this.dailyLimit = dailyLimit;
+  this.withdrawnToday = withdrawntoday;
+}
+
+  
+}
